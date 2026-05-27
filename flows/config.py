0ab09826f201d9fc1367e04daf96709971b6db2b
marlo-80 <erster.neuer@gmail.com>
2026-05-27 11:35:10 +0200

    clean

--- flows/config.py
+++ flows/config.py
@@ -5,7 +5,17 @@
 
 DEFAULT_CONFIG = {
     "run_name": "small_rf",
+
+    # Data Definitions (Logging only)
     "dataset_query": "SELECT * FROM dbt_staging.flights_subset",
+    "dataset_name": "flights_subset_2019-2020",
+    "dataset_start_date": "2019-01-01",
+    "dataset_end_date": "2020-01-01",
+    "dataset_sample_size": 100000,
+    "dataset_random_seed": 0.42,
+    "dataset_source": "dbt_staging.flights_subset",
+    
+    # Feature Definitions 
     "target": "arr_delay_minutes",
     "numeric_cols": [
         "year", "quarter", "month", "day_of_month", "day_of_week",
@@ -14,20 +24,23 @@ DEFAULT_CONFIG = {
     ],
     "categorical_cols": [
     ],
+
+    # Preprocessing Definition
     "impute_num": "median",
     "impute_cat": "most_frequent",
+
+    # Model Definitions
     "model_type": "RandomForestRegressor",
     "model_params": {"n_estimators": 50, "max_depth": 10, "random_state": 42},
+    
+
+
+
+    # Registration and Evaluation
     "register": False,
-    "model_name": "flight-delay-baseline",
+    "model_name": "flight-delay-prediction",
     "alias": "",
     "delay_threshold": 15,
-    "dataset_name": "flights_subset_2019-2020",
-    "dataset_start_date": "2019-01-01",
-    "dataset_end_date": "2020-01-01",
-    "dataset_sample_size": 100000,
-    "dataset_random_seed": 0.42,
-    "dataset_source": "dbt_staging.flights_subset",
 }
 
 
@@ -42,32 +55,6 @@ OPTUNA_CONFIG = {
     "n_trials": 30,
     "direction": "minimize",          # "minimize" für RMSE
 
-    # Feature Definition
-    "target": "arr_delay",
-    "numeric_cols": [
-        "year", "quarter", "month", "day_of_month", "day_of_week",
-        "crs_dep_time", "crs_arr_time", "crs_elapsed_time",
-        "distance", "distance_group",
-    ],
-    "categorical_cols": [
-        "origin", "origin_city_name", "origin_state",
-        "dest", "dest_city_name", "dest_state",
-        "marketing_airline_network", "operating_airline",
-        "flight_number_marketing_airline", "flight_number_operating_airline",
-        "tail_number",
-        "dep_time_blk",
-    ],
-    "impute_num": "median",
-    "impute_cat": "most_frequent",
-    
-    # Model Definition
-    "model_type": "RandomForestRegressor",
-    "param_ranges": {
-        "n_estimators": {"type": "int", "low": 5, "high": 50},
-        "max_depth":     {"type": "int", "low": 2, "high": 6},
-        # später erweiterbar: "learning_rate": {"type": "float", "low": 1e-3, "high": 0.1, "log": True}
-    },
-
     # Data Definition
     "dataset_query": "SELECT * FROM dbt_staging.flights_subset",
     "dataset_source": "dbt_staging.flights_subset",
@@ -76,7 +63,27 @@ OPTUNA_CONFIG = {
     "dataset_end_date": "2020-01-01",
     "dataset_sample_size": 100000,
     "dataset_random_seed": 0.42,
+
+    # Feature Definition
+    "target": "arr_delay",
+    "numeric_cols": [
+        "year", "quarter", "month", "day_of_month", "day_of_week",
+        "crs_dep_time", "crs_arr_time", "crs_elapsed_time",
+        "distance", "distance_group",
+    ],
+    "categorical_cols": [
+    ],
+
+    # Preprocessor Definitions
+    "impute_num": "median",
+    "impute_cat": "most_frequent",
     
+    # Model Definition
+    "model_type": "RandomForestRegressor",
+    "param_ranges": {
+        "n_estimators": {"type": "int", "low": 5, "high": 50},
+        "max_depth":     {"type": "int", "low": 2, "high": 6},
+    },
 
     # Registration and Evaluation
     "register": False,
